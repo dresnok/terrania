bc9c01272ece1c09c191083f95ea0b205e0e3514
dresnok <marcin.dresnok@gmail.com>
2026-07-25 20:14:55 +0200

    pierwszy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,16 @@
-import { defineConfig } from "astro/config";
-import vue from "@astrojs/vue";
+// @ts-check
+import { defineConfig } from 'astro/config';
 
+import vue from '@astrojs/vue';
+
+// https://astro.build/config
 export default defineConfig({
   integrations: [vue()],
+  
+
+  
+
+  devToolbar: {
+    enabled: true
+  },
 });
